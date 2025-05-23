9323937420fa59ed9488d73d6a3e55e89d765fcd
ApfelNektar <149399980+AnnesMuhamed@users.noreply.github.com>
2025-05-23 13:43:17 +0200

    Implemented coin and bottle collection system with status bars, including collision detection and percentage-based progress tracking for both collectibles.

--- modals/coins.class.js
+++ modals/coins.class.js
@@ -5,12 +5,20 @@ class coins extends CollectibleObjects {
   IMAGES_COINS = ["img/8_coin/coin_1.png", "img/8_coin/coin_2.png"];
 
   constructor() {
-    super().loadImage('img/8_coin/coin_1.png');
+    super().loadImage(this.IMAGES_COINS[0]);
     this.loadImages(this.IMAGES_COINS);
 
     // Münzen werden zwischen 200 und 1800 platziert, mit mindestens 200px Abstand zum Endboss
-    this.x = 200 + (Math.random() * 1600) * 1;
+    this.x = 200 + Math.random() * 1600 * 1;
     this.height = 80;
     this.width = 80;
+
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.playAnimation(this.IMAGES_COINS);
+    }, 200);
   }
 }
--- modals/world.class.js
+++ modals/world.class.js
@@ -1,108 +1,130 @@
 class World {
-    character = new Character();  // Innerhalb eines Objekts braucht man keine let - var- const.
-    level = level1;
-    canvas;
-    ctx;
-    keyboard;
-    camera_x = -0;
-    statusBar = new StatusBar();
-    statusBarBottle = new StatusBarBottle();
-    statusBarCoin = new StatusBarCoin();
-    
+  character = new Character(); // Innerhalb eines Objekts braucht man keine let - var- const.
+  level = level1;
+  canvas;
+  ctx;
+  keyboard;
+  camera_x = -0;
+  statusBar = new StatusBar();
+  statusBarBottle = new StatusBarBottle();
+  statusBarCoin = new StatusBarCoin();
 
-    constructor (canvas, keyboard) {
-        this.ctx = canvas.getContext('2d');
-        this.canvas = canvas; // Varibale wird in parameter geschrieben obwohl das die gleiche schreibweise ist.
-        this.keyboard = keyboard;
-        this.draw();
-        this.setWorld();
-        this.checkCollisions();
+  constructor(canvas, keyboard) {
+    this.ctx = canvas.getContext("2d");
+    this.canvas = canvas; // Varibale wird in parameter geschrieben obwohl das die gleiche schreibweise ist.
+    this.keyboard = keyboard;
+    this.draw();
+    this.setWorld();
+    this.checkCollisions();
+  }
+
+  setWorld() {
+    this.character.world = this;
+  }
+
+  checkCollisions() {
+    setInterval(() => {
+      this.checkEnemyCollisions();
+      this.checkCoinCollisions();
+      this.checkBottleCollisions();
+    }, 200);
+  }
+
+  checkEnemyCollisions() {
+    this.level.enemies.forEach((enemy) => {
+      if (this.character.isColliding(enemy)) {
+        this.character.hit();
+        this.statusBar.setPercentage(this.character.energy);
+      }
+    });
+  }
+
+  checkCoinCollisions() {
+    this.level.coins.forEach((coin, index) => {
+      if (this.character.isColliding(coin)) {
+        this.level.coins.splice(index, 1);
+        this.statusBarCoin.setCoinStatusbarPercentage(
+          this.statusBarCoin.coinStatusbarPercentage + 20
+        );
+      }
+    });
+  }
+
+  checkBottleCollisions() {
+    this.level.bottle.forEach((bottle, index) => {
+      if (this.character.isColliding(bottle)) {
+        this.level.bottle.splice(index, 1);
+        this.statusBarBottle.setBottleStatusbarPercentage(
+          this.statusBarBottle.bottleStatusbarPercentage + 20
+        );
+      }
+    });
+  }
+
+  draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // Löscht das vorherige bild.
+
+    this.ctx.translate(this.camera_x, 0); // bild verschiebt sich nach links wenn sich der charackter bewegt.
+    this.addObjectsToMap(this.level.backgroundObjects);
+
+    this.addToMap(this.character);
+    this.addObjectsToMap(this.level.clouds);
+    this.addObjectsToMap(this.level.enemies);
+    this.addObjectsToMap(this.level.coins);
+    this.addObjectsToMap(this.level.bottle);
+
+    this.ctx.translate(-this.camera_x, 0);
+    // Space for fixed Objects
+    this.addToMap(this.statusBar);
+    this.ctx.translate(this.camera_x, 0);
+
+    this.ctx.translate(-this.camera_x, 0);
+    // Space for fixed Objects
+    this.addToMap(this.statusBarCoin);
+    this.ctx.translate(this.camera_x, 0);
+
+    this.ctx.translate(-this.camera_x, 0);
+    // Space for fixed Objects
+    this.addToMap(this.statusBarBottle);
+    this.ctx.translate(this.camera_x, 0);
+
+    this.ctx.translate(-this.camera_x, 0);
+
+    let self = this;
+    requestAnimationFrame(function () {
+      // Häufig Aufrufen.
+      self.draw();
+    });
+  }
+
+  addObjectsToMap(objects) {
+    objects.forEach((o) => {
+      this.addToMap(o);
+    });
+  }
+
+  addToMap(mo) {
+    if (mo.otherDirection) {
+      this.flipImage(mo);
     }
 
-    setWorld() {
-        this.character.world = this;
+    mo.draw(this.ctx);
+    mo.drawFrame(this.ctx);
+
+    if (mo.otherDirection) {
+      this.flipImageBack(mo);
     }
+  }
 
-    checkCollisions() {
-        setInterval(() => {
-            this.level.enemies.forEach((enemy) => {
-               if (this.character.isColliding(enemy)) {
-                    this.character.hit();
-                    this.statusBar.setPercentage(this.character.energy);
-               }
-            });
-        }, 200);
-    }
+  flipImage(mo) {
+    this.ctx.save();
+    this.ctx.translate(mo.width, 0);
+    this.ctx.scale(-1, 1);
+    mo.x = mo.x * -1;
+  }
 
-    draw() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // Löscht das vorherige bild. 
-
-        this.ctx.translate(this.camera_x, 0); // bild verschiebt sich nach links wenn sich der charackter bewegt.
-        this.addObjectsToMap(this.level.backgroundObjects);
-
-        this.addToMap(this.character);
-        this.addObjectsToMap(this.level.clouds);
-        this.addObjectsToMap(this.level.enemies);
-        this.addObjectsToMap(this.level.coins);
-        this.addObjectsToMap(this.level.bottle);
-        
-
-        this.ctx.translate(-this.camera_x, 0);
-        // Space for fixed Objects
-        this.addToMap(this.statusBar);
-        this.ctx.translate(this.camera_x, 0);
-
-        this.ctx.translate(-this.camera_x, 0);
-        // Space for fixed Objects
-        this.addToMap(this.statusBarCoin);
-        this.ctx.translate(this.camera_x, 0);
-
-        this.ctx.translate(-this.camera_x, 0);
-        // Space for fixed Objects
-        this.addToMap(this.statusBarBottle);
-        this.ctx.translate(this.camera_x, 0);
-
-        
-
-        
-
-        this.ctx.translate(-this.camera_x, 0);
-        
-
-        let self = this;
-        requestAnimationFrame(function() { // Häufig Aufrufen.
-            self.draw();
-        }); 
-    }
-
-    addObjectsToMap(objects) {
-        objects.forEach(o => {
-            this.addToMap(o);
-        });
-    }
-
-    addToMap(mo) {
-        if(mo.otherDirection) {
-            this.flipImage(mo);
-        }
-
-        mo.draw(this.ctx);
-        mo.drawFrame(this.ctx);
-
-        if(mo.otherDirection) {
-            this.flipImageBack(mo);
-        }
-    }
-
-    flipImage(mo) {
-        this.ctx.save();
-        this.ctx.translate(mo.width, 0);
-        this.ctx.scale(-1, 1);
-        mo.x = mo.x * -1;
-    }
-
-    flipImageBack(mo) {
-        mo.x = mo.x * -1;
-        this.ctx.restore();
-    }
+  flipImageBack(mo) {
+    mo.x = mo.x * -1;
+    this.ctx.restore();
+  }
 }
